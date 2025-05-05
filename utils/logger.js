212038b7fd04cb025a36186d23d46621cb5c407a
Beleke Ian <ianseeksbeta@gmail.com>
2025-05-05 13:10:26 +0100

    Creation of logger utility

---
+++ utils/logger.js
@@ -0,0 +1,77 @@
+import winston from "winston";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const logDir = path.join(__dirname, "../logs")
+
+const { format: {
+    combine,
+    timestamp,
+    colorize,
+    prettyPrint,
+    errors,
+    align
+} } = winston
+
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === "development" ? "info" : "debug",
+    format: combine(
+        timestamp({format: "YYYY:-MM-DD HH:mm:ss"}),
+        errors({stack: true}),
+        prettyPrint()
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: path.join(logDir, 'app.log'),
+            level: "info",
+            maxsize: 5242880,
+            maxFiles: 5,
+        }),
+        new winston.transports.File({
+            filename: path.join(logDir, 'error.log'),
+            level: "error",
+            maxsize: 5242880, // 5MB
+            maxFiles: 5
+        })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({
+            filename: path.join(logDir, 'exceptions.log'),
+            level: "error",
+            maxsize: 5242880, // 5MB
+            maxFiles: 5
+        })
+    ],
+    rejectionHandlers: [
+        new winston.transports.File({
+            filename: path.join(logDir, 'rejections.log'),
+            level: "error",
+            maxsize: 5242880, // 5MB
+            maxFiles: 5
+        })
+    ],
+    exitOnError: false
+})
+
+
+if(process.env.NODE_ENV !== "production") {
+    logger.add(new winston.transports.Console({
+        level: "debug",
+        format: combine(
+        colorize(),
+        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        align()
+      )
+    }))
+}
+
+logger.stream = {
+    write: (message) => {
+      logger.info(message.substring(0, message.lastIndexOf("\n")))
+    }
+}
+
+export default logger
