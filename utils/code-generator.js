9d54a09cc25a2b4383ad66915dca7bbf1c23fc92
Beleke Ian <ianseeksbeta@gmail.com>
2025-05-07 23:16:23 +0100

    Crypto fix for server side development

--- utils/code-generator.js
+++ utils/code-generator.js
@@ -1,12 +1,16 @@
+import crypto from 'crypto'; // Make sure this is at the top of the file
+
 export function generateCode(length = 6, charset = 'alphanumeric') {
     const charsets = {
-      alphanumeric: 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789', // Excluded easily confused chars
-      numeric: '0123456789',
-      alphabetic: 'ABCDEFGHJKLMNPQRSTUVWXYZ'
+        alphanumeric: 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789',
+        numeric: '0123456789',
+        alphabetic: 'ABCDEFGHJKLMNPQRSTUVWXYZ'
     };
-    
+
     const chars = charsets[charset] || charsets.alphanumeric;
-    return Array.from(crypto.getRandomValues(new Uint8Array(length)))
-      .map(byte => chars[byte % chars.length])
-      .join('');
-  }
+    const randomBytes = crypto.randomBytes(length);
+
+    return Array.from(randomBytes)
+        .map(byte => chars[byte % chars.length])
+        .join('');
+}
