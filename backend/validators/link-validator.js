4729c37d94005132df6b62e2cdce1d9c4a7647c1
Beleke Ian <ianseeksbeta@gmail.com>
2025-05-10 11:50:25 +0100

    Added entries for custom codes and expiration dates to url. As well as adjustment of code lenght

--- backend/validators/link-validator.js
+++ backend/validators/link-validator.js
@@ -6,9 +6,9 @@ const linkValidator = Joi.object({
     'string.uri': 'The longUrl must be a valid URI.',
     'any.required': 'The longUrl field is required.',
   }),
-  customCode: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,6}$')).optional().messages({
+  customCode: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{4,6}$')).optional().messages({
     'string.base': 'The customCode must be a string.',
-    'string.pattern.base': 'The customCode must be 3-30 characters long and contain only letters and numbers.',
+    'string.pattern.base': 'The customCode must be 4 - 6 characters long and contain only letters and numbers.',
   }),
   expiresAt: Joi.date().iso().greater('now').optional().messages({
     'date.base': 'The expiresAt must be a valid date.',
